e3977c16a31ca69ccceed8aa2e936d83de739a61
ChristianAlonso16 <20213tn050@utez.edu.mx>
2024-02-08 16:37:26 -0600

    Chris

---
+++ src/router/router.js
@@ -0,0 +1,12 @@
+import VueRouter from "vue-router";
+import Vue from "vue";
+
+Vue.use(VueRouter);
+const routes = [
+  {
+    path: "/",
+    component: () => import("../views/list_books.vue"),
+  },
+];
+const router = new VueRouter({ routes });
+export default router;
